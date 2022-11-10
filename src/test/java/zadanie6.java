3ec2e681a0f3c93de7329816235c95aed19ebec6
RallyDaria <dariazblewska@gmail.com>
2022-11-10 21:03:44 +0100

    zadanko6

---
+++ src/test/java/zadanie6.java
@@ -0,0 +1,20 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class zadanie6 {
+
+    private static   WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testZadanie6(){
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.findElement(By.className("oxd-input oxd-input--active")).sendKeys("Admin");
+    }
+}
