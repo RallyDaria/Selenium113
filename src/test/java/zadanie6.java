49a5ea0fee98713a2cc979298e3e5ffc3a1ecb84
RallyDaria <dariazblewska@gmail.com>
2022-11-14 17:56:13 +0100

    klik button

--- src/test/java/zadanie6.java
+++ src/test/java/zadanie6.java
@@ -4,17 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class zadanie6 {
 
     private static   WebDriver driver;
     @BeforeClass
     public static void setUp(){
+
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Test
     public void testZadanie6(){
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        driver.findElement(By.className("oxd-input oxd-input--active")).sendKeys("Admin");
+        //driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+        //driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
     }
+
 }
+
