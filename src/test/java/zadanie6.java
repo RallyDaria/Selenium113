09efc1caf7aea668c7f57fb3678667fe3fee12b7
RallyDaria <dariazblewska@gmail.com>
2022-11-14 19:57:46 +0100

    find element liczenie linków i zdjęć na stronie  56+

--- src/test/java/zadanie6.java
+++ src/test/java/zadanie6.java
@@ -1,16 +1,19 @@
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class zadanie6 {
 
     private static   WebDriver driver;
-    @BeforeClass
-    public static void setUp(){
+    private  String url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+private String urlBBC = " https://www.bbc.com.";
+    @Before
+    public void setUp(){
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -19,12 +22,56 @@ public class zadanie6 {
 
     @Test
     public void testZadanie6(){
-        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.navigate().to(url);
+
         //driver.findElement(By.name("username")).sendKeys("Admin");
         driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
         //driver.findElement(By.name("password")).sendKeys("admin123");
         driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
         driver.findElement(By.tagName("button")).click();
+        Assert.assertEquals("Dashboard",driver.findElement(By.tagName("h6")).getText());
+        System.out.println("Udało się zalogować");
+        //driver.close();
+
+    }
+    @Test
+    public void testZadanie6_1() {
+        driver.navigate().to(url);
+
+        //driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+        //driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
+        Assert.assertEquals("Dashboard", driver.findElement(By.tagName("h6")).getText());
+        System.out.println("Udało się zalogować");
+        //driver.close();
+    }
+    @Test
+    public void zadanie8() throws InterruptedException {
+        driver.navigate().to(urlBBC);
+        Thread.sleep(1000);
+        WebElement link = driver.findElement(By.tagName("a"));
+        System.out.println(link.getText());
+       List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println("ilość linków na stronie = " + links.size());
+         List<WebElement>  immages = driver.findElements(By.tagName("img"));
+        System.out.println("ilość obrazków na stronie =  " + immages.size());
+
+        System.out.println(immages.get(1).getText());
+
+
+        int[] tablica ={1,2,3};
+        System.out.println(tablica[1]);
+
+
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+
     }
 
 }
